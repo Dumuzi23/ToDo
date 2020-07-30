23bebf5486f02571791b3e6de5ad31584f884c6a
ta <amida.tatsuya@gmail.com>
2020-07-30 20:40:29 +0900

    feat-CoreDataで保存したデータをロードできるようにしました

--- toDo/toDo/TodoListViewController.swift
+++ toDo/toDo/TodoListViewController.swift
@@ -7,19 +7,20 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print(dataFilePath)
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
 
-//        loadItems()
+        loadItems()
 
     }
 
@@ -102,16 +103,14 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
 
-//    func loadItems() {
-//        if let data = try? Data(contentsOf: dataFilePath!) {
-//            let decoder = PropertyListDecoder()
-//            do {
-//                itemArray = try decoder.decode([Item].self, from: data)
-//            } catch {
-//                print("Error decoding item array, \(error)")
-//            }
-//        }
-//    }
+    func loadItems() {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+    }
 
 }
 
