0699d0180f2819049f405169d500aad6ed676bea
ta <amida.tatsuya@gmail.com>
2020-08-03 21:10:07 +0900

    feat-Categoryを追加する機能を追加しました

--- toDo/toDo/CategoryViewController.swift
+++ toDo/toDo/CategoryViewController.swift
@@ -7,16 +7,92 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
+
+    var categories = [Category]()
+
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loadCategories()
     }
 
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        
+    //MARK: - TableView Datasource Methods
+
+    // 行数を指定するメソッド（必須）
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
     }
+
+    // セルを作成し、tableViewに返すメソッド（必須）
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "CategoryCell")
+
+        let category = categories[indexPath.row]
+
+        cell.textLabel?.text = category.name
+
+        return cell
+    }
+
+    //MARK: - Data Manipulation Methods
+    func saveCategories() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving category, \(error)")
+        }
+
+        self.tableView.reloadData()
+    }
+
+    func loadCategories() {
+        let request: NSFetchRequest<Category> = Category.fetchRequest()
+
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error loading categories \(error)")
+        }
+
+        tableView.reloadData()
+    }
+
+    //MARK: - Add New Category
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            // AlertのAdd Categoryボタンを押した時の動作
+            let newCategory = Category(context: self.context)
+
+            // textFieldのtextプロパティがnilになることは無いので、forced unwrapして良い
+            newCategory.name = textField.text!
+
+            self.categories.append(newCategory)
+
+            self.saveCategories()
+        }
+        
+        // Alertにテキストフィールドを追加
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Add new Category"
+            textField = alertTextField
+        }
+
+        alert.addAction(action)
+
+        present(alert, animated: true, completion: nil)
+    }
+
+    //MARK: - TableView Delegate Methods
 
 }
